19b8c8eba784fde5373c7315011cdd15c2b2ea7e
Riccarpa <riccardopagni99@gmail.com>
2021-07-28 16:33:31 +0200

    import files

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+/*
+Descrizione:
+Rifare l'esercizio dello slider come fatto assieme in classe.
+Bonus:
+Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine 
+automaticamente.
+IMPORTANTE: Non perdete troppo tempo sui dettagli grafici,
+ l'importante sono le funzionalità , non riterremo  errori se la grafica
+ non rispecchia in maniera identica quello che abbiamo fatto in classe
+*/
+
+
+
+
+
 Vue.config.devtools = true;
 
 const root = new Vue({
